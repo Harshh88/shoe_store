416cfdbd067a02e7699ab8e46ddfad09610cd20d
Harsh Kashyap <harshkashyap@Harshs-MacBook.local>
2026-05-29 14:52:49 +0530

    added new feature in db

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,15 @@
-const {Pool} = require("pg");
-// const dotenv = require("dotenv");
-// dotenv.config({ path: "../.env" });
+const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
