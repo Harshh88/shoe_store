ec428ab6a1f9ac45a67d7b8cc94c2bed9e43b25e
Harshh88 <hkashyap816@gmail.com>
2026-06-10 14:43:19 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -7,9 +7,9 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
 
-  // ssl: {
-  //   rejectUnauthorized: false,
-  // },
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
