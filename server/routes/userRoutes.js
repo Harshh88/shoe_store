18389cba5bd8ef37af1290bd83c126c3db7fb17e
Harshh88 <hkashyap816@gmail.com>
2026-06-16 20:21:22 +0530

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,1 @@
-const {getOneUser} = require("../controllers/userController");
-const express = require("express");
-const router = express.Router();
-const auth = require("../middleware/auth");
 
-router.post("/get-profile",auth,getOneUser);
-
-module.exports = router;
