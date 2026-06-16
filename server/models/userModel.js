1b72b63363f21799f04997127b973961f6a2831a
Harshh88 <hkashyap816@gmail.com>
2026-06-17 00:41:01 +0530

    Update userModel.js

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -2,25 +2,24 @@ const pool = require("../config/db");
 
 const createTable = async () => {
   try {
-    
     await pool.query(`
       CREATE TABLE IF NOT EXISTS users(
         id SERIAL PRIMARY KEY,
         name VARCHAR(30) NOT NULL,
         email VARCHAR(100) UNIQUE NOT NULL,
+        phone VARCHAR(20),
         role VARCHAR(20) DEFAULT 'USER' CHECK(role IN ('USER','ADMIN','SELLER')),
         password VARCHAR(255) NOT NULL,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
       )  
     `);
 
-    
     await pool.query(`
       ALTER TABLE users
+      ADD COLUMN IF NOT EXISTS phone VARCHAR(20),
       ADD COLUMN IF NOT EXISTS image_id INTEGER
     `);
 
-    
     await pool.query(`
       DO $$
       BEGIN
@@ -39,12 +38,10 @@ const createTable = async () => {
     `);
 
     console.log("User table and constraints synchronized successfully!");
-
   } catch (err) {
     console.log("something error in userModel", err);
     throw err; 
   }
 };
 
-
-module.exports =  createTable ;
+module.exports = createTable;
