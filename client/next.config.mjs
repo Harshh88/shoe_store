cb00ed7627672f2ea4784ad979e63aab49182e07
Harshh88 <hkashyap816@gmail.com>
2026-06-16 22:52:57 +0530

    Update next.config.mjs

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://shoestore-production-3d6f.up.railway.app/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
