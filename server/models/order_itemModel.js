b55283abca8297f26c870c0e9019482e23ab9f5b
Harshh88 <hkashyap816@gmail.com>
2026-06-27 16:52:19 +0530

    Update order_itemModel.js

--- server/models/order_itemModel.js
+++ server/models/order_itemModel.js
@@ -20,6 +20,7 @@ const createTable = async () => {
     `);
 
     await pool.query(`
+      ALTER TABLE order_items DROP CONSTRAINT IF EXISTS order_items_product_id_fkey;
       ALTER TABLE order_items DROP CONSTRAINT IF EXISTS fk_order_items_product;
       ALTER TABLE order_items 
       ADD CONSTRAINT fk_order_items_product 
